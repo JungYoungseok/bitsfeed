d1678d29d5f159bd46f2db3e3427cad106e881b9
jacky.jung <jacky.jung@datadoghq.com>
2025-05-11 20:56:41 +0900

    스케쥴 조정

--- datadog-crawler/api/main.py
+++ datadog-crawler/api/main.py
@@ -2,6 +2,8 @@ from fastapi import FastAPI
 from crawler.google_news import fetch_google_news
 from db.mongodb import insert_news, get_all_news
 from fastapi.middleware.cors import CORSMiddleware
+import schedule
+import time
 
 app = FastAPI()
 app.add_middleware(
@@ -26,3 +28,9 @@ def crawl_news():
     news_items = fetch_google_news()
     insert_news(news_items)
     return {"inserted": len(news_items)}
+
+# 매일 오전 9시에 실행
+schedule.every().day.at("09:00").do(fetch_google_news)
+while True:
+    schedule.run_pending()
+    time.sleep(60)
