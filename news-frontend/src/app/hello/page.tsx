962fe1cbc6fbbafa13906755f0af6f1367ae98b9
jacky.jung <jacky.jung@datadoghq.com>
2025-05-18 12:40:50 +0900

    add hello healthcheck page

---
+++ news-frontend/src/app/hello/page.tsx
@@ -0,0 +1,10 @@
+// src/app/hello/page.tsx
+
+export default function HelloPage() {
+    return (
+      <div style={{ padding: "2rem", fontFamily: "sans-serif" }}>
+        <h1>✅ Hello from news-frontend</h1>
+      </div>
+    );
+  }
+  
