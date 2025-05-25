14fb77263483fa5fce8441bb61c85a9cf6773245
jacky.jung <jacky.jung@datadoghq.com>
2025-05-25 22:52:13 +0900

    rum apm trace connect

--- news-frontend/src/lib/datadog-rum-init.tsx
+++ news-frontend/src/lib/datadog-rum-init.tsx
@@ -7,34 +7,22 @@ import { datadogRum } from '@datadog/browser-rum';
 export function DatadogRUM() {
 
   datadogRum.init({
-    applicationId: '57bf6486-cd46-430c-aacd-0b16764c78ba',
-    clientToken: 'pub29626c776ade9f7b38b80464fe9a56a7',
-    site: 'datadoghq.com',
+    applicationId: process.env.NEXT_PUBLIC_DD_RUM_APP_ID!,
+    clientToken: process.env.NEXT_PUBLIC_DD_RUM_CLIENT_TOKEN!,
+    site: (process.env.NEXT_PUBLIC_DD_RUM_SITE as "datadoghq.com") || "datadoghq.com",
     service: 'news-frontend',
     env: 'prod',
     version: '1.0.0',
     sessionSampleRate: 100,
     sessionReplaySampleRate: 100,
     defaultPrivacyLevel: 'allow',
+    allowedTracingUrls: [
+      process.env.NEXT_PUBLIC_DD_TRACINGURL_1 || "",
+      process.env.NEXT_PUBLIC_DD_TRACINGURL_2 || "",
+    ],
   });
-
-
-    // datadogRum.init({
-    //   applicationId: process.env.NEXT_PUBLIC_DD_RUM_APP_ID!,
-    //   clientToken: process.env.NEXT_PUBLIC_DD_RUM_CLIENT_TOKEN!,
-    //   site: (process.env.NEXT_PUBLIC_DD_RUM_SITE as "datadoghq.com") || "datadoghq.com",
-     
-    //   service:'datadog-news-front',
-    //   env: 'prod',
-    //   version: "1.0.0",
-    //   sessionSampleRate: 100,
-    //   sessionReplaySampleRate: 100,
-    //   defaultPrivacyLevel: 'allow',
-    //   plugins: [reactPlugin({ router: true })],
-    // });
-
-    
+   
     // no-dd-sa
-    console.log("🔥 Datadog RUM initialized");
-    return null;
+  console.log("🔥 Datadog RUM initialized");
+  return null;
 }
