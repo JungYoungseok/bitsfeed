c468a743257f1753de1962ac58a156938255ce13
jacky.jung <jacky.jung@datadoghq.com>
2025-06-15 15:58:02 +0900

    5개까지 읽어오도록 조정

--- datadog-crawler/crawler/google_news.py
+++ datadog-crawler/crawler/google_news.py
@@ -6,19 +6,7 @@ from datetime import datetime
 import urllib.parse
 from dateutil import parser
 import requests
-
-
-# def extract_article_text(url: str) -> str:
-#     try:
-#         from newspaper import Article 
-#         article = Article(url, language='en')  # 또는 'ko' for Korean news
-#         article.download()
-#         article.parse()
-#         return article.text
-#     except Exception as e:
-#         print(f"❌ 기사 본문 추출 실패: {e}")
-#         return ""
-    
+   
 def resolve_redirect_url(google_link: str) -> str:
     try:
         res = requests.get(google_link, timeout=5, allow_redirects=True)
@@ -35,7 +23,7 @@ def fetch_google_news():
     print(f"\n[Datadog 뉴스 업데이트 - {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}]")
     
     results = []
-    for entry in feed.entries[:3]:  # 최근 3개만 출력
+    for entry in feed.entries[:5]:  # 최근 5개만 출력
         title = entry.title
         original_link = entry.link
         resolved_link = resolve_redirect_url(original_link)  # ✅ 여기서 실제 기사 링크로 변환
